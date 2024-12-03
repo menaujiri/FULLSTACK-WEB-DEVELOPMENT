c351ab59ba11d0c09fe802a61842951ca1bd566b
menaujiri <ujirimena@gmail.com>
2024-12-03 21:56:32 +0100

    next sequence function

--- 1. Front-End Development/20. Simon Game Challenge Starting Files/game.js
+++ 1. Front-End Development/20. Simon Game Challenge Starting Files/game.js
@@ -1,11 +1,11 @@
-gamePattern = []
 
 var bottonColours = ["red", "blue", "green", "yellow"]
 
+var gamePattern = []
+
 function nextSequence() {
-    var randonNumber = Math.floor(Math.random() *3);
+    var randonNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = bottonColours[randonNumber];
+    gamePattern.push(randomChosenColour);
 }
 
-var randomChosenColour = bottonColours[randonNumber]
-
-gamePattern.push(randomChosenColour)
