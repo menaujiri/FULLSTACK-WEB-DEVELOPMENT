af8d87d9541e74d72a2f0dcb25a8482b8b31f25c
menaujiri <ujirimena@gmail.com>
2024-12-03 21:27:47 +0100

    created gamePattern to choose a random colour

---
+++ 1. Front-End Development/20. Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,11 @@
+gamePattern = []
+
+var bottonColours = ["red", "blue", "green", "yellow"]
+
+function nextSequence() {
+    var randonNumber = Math.floor(Math.random() *3);
+}
+
+var randomChosenColour = bottonColours[randonNumber]
+
+gamePattern.push(randomChosenColour)
