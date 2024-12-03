16a82f1a210fafa0cfcd24f771df2372cdd46fc8
menaujiri <ujirimena@gmail.com>
2024-12-04 00:11:34 +0100

    Next sequence

--- 1. Front-End Development/20. Simon Game Challenge Starting Files/game.js
+++ 1. Front-End Development/20. Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,8 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
+var userClickedPattern = [];
+
 
 
 
@@ -8,5 +10,13 @@ function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChoosenNumber = buttonColours[randomNumber];
     gamePattern.push(randomChoosenNumber);
+    $("#" + randomChoosenNumber).fadeIn(100).fadeOut(100).fadeIn(100);
+    var audio = new Audio('sounds/' + randomChoosenNumber + '.mp3');
+    audio.play();
+    $(".btn").click(function() {
+        var userChosenColour = $(this).attr("id");
+        // Add further code to handle the user's chosen colour
+    });
+    userClickedPattern.push(userChosenColour);
 }
 
