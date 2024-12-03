362c1d146267337527321619a97e559c0c4ad06e
menaujiri <ujirimena@gmail.com>
2024-12-03 23:22:23 +0100

    create a random number

--- 1. Front-End Development/20. Simon Game Challenge Starting Files/game.js
+++ 1. Front-End Development/20. Simon Game Challenge Starting Files/game.js
@@ -1,16 +1,4 @@
-
-
-
-var bottonColours = ["red", "blue", "green", "yellow"]
-
-var gamePattern = []
-var userClickedPattern = []
-
-$(".btn").click(function() {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    new audio = new Audio("sounds/" + userChosenColour + ".mp3");
-    audio.play();
-    animatePress(userChosenColour);
-})
+function nextSequence() {
+    var randonNumber = Math.floor(Math.random() * 4);
+}
 
