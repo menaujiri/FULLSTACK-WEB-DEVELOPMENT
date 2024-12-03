82e9b07758a6d82835d38a1cd90d5a2934bd1b4d
menaujiri <ujirimena@gmail.com>
2024-12-03 23:32:57 +0100

    Added randon choosen number

--- 1. Front-End Development/20. Simon Game Challenge Starting Files/game.js
+++ 1. Front-End Development/20. Simon Game Challenge Starting Files/game.js
@@ -1,4 +1,12 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+
+
 function nextSequence() {
-    var randonNumber = Math.floor(Math.random() * 4);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChoosenNumber = buttonColours[randomNumber];
+    gamePattern.push(randomChoosenNumber);
 }
 
