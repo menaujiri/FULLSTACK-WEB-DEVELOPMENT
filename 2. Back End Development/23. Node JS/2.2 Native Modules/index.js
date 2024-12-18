37616e1a12725485bf08e2ac92f8ddeca8c2e05c
menaujiri <ujirimena@gmail.com>
2024-12-18 02:42:51 +0100

    npm packages

--- 2. Back End Development/23. Node JS/2.2 Native Modules/index.js
+++ 2. Back End Development/23. Node JS/2.2 Native Modules/index.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 
-fs.writeFile('example.txt', 'This is an example',(err) => {
+// fs.writeFile('example.txt', 'This is an example',(err) => {
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log('File successfully created');
+// });
+
+fs.readFile('example.txt', 'utf8', (err, file) => {
     if(err)
         console.log(err);
     else
-        console.log('File successfully created');
+        console.log(file);
 });
-    
