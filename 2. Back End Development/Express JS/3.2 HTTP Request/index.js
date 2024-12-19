9825736ab36f9868a719d9120cca8ab60639ec0c
menaujiri <ujirimena@gmail.com>
2024-12-19 04:02:46 +0100

    send contact me

--- 2. Back End Development/Express JS/3.2 HTTP Request/index.js
+++ 2. Back End Development/Express JS/3.2 HTTP Request/index.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/about', (req, res) => {
+    res.send("<h1>About Us</h1>")
+});
+
+app.get('/contact', (req, res) => {
+    res.send("<h1>Contact Us</h1>")
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
