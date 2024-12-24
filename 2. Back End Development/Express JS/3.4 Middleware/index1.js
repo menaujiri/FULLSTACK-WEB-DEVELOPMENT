a3a9800f8e52dd98ccdf9670ef1161a84a901242
menaujiri <ujirimena@gmail.com>
2024-12-24 03:27:31 +0100

    update

--- 2. Back End Development/Express JS/3.4 Middleware/index1.js
+++ 2. Back End Development/Express JS/3.4 Middleware/index1.js
@@ -1,7 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const app = express();
 const port = 3000;
@@ -9,6 +13,10 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
+app.post("/submit", (res, req) => {
+  console.log(req.body);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
